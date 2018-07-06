d67a4afc47eaf4c07afd33da188da0e1c785becf
Theodis Thompson <theothomp37@gmail.com>
2018-07-05 22:21:10 -0400

    typescript services

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from './UserService';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) { }
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      fname: ['', Validators.required],
+      lname: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
+  get f() {return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    // stop if invalid form
+    if (this.registerForm.invalid) {
+      return;
+    }
+  }
 }
